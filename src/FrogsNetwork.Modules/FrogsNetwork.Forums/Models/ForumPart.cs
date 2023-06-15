f9bbbe1c3903dbb4d8b290e83124a3cc45a8da85
digisaman <saman.pirooz@live.com>
2023-06-15 11:46:13 +0330

    ForumPart Added

---
+++ src/FrogsNetwork.Modules/FrogsNetwork.Forums/Models/ForumPart.cs
@@ -0,0 +1,25 @@
+using OrchardCore.ContentManagement;
+
+namespace FrogsNetwork.Forums.Models;
+
+public class ForumPart : ContentPart
+{   
+    //[StringLengthMax]
+    public virtual string Description { get; set; } = "";
+
+    public virtual int ThreadCount { get; set; }
+    public virtual int PostCount { get; set; }
+
+    public virtual bool ThreadedPosts { get; set; }
+
+    public virtual int Weight { get; set; }
+
+    public virtual int ReplyCount
+    {
+        get { return PostCount >= ThreadCount ? PostCount - ThreadCount : 0; }
+    }
+
+    public string PostContentType { get; set; } = "Post";
+    public List<ContentItem> Posts { get; set; } = new List<ContentItem>();
+
+}
