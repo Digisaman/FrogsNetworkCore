0d61a1f3db49d4dfc942fa16c41063ad4e946820
digisaman <saman.pirooz@live.com>
2023-08-03 22:15:14 +0300

    Main Menu Dashboard lik based on role fixed

--- src/OrchardCore.Themes/TheTheme/MainMenu.cs
+++ src/OrchardCore.Themes/TheTheme/MainMenu.cs
@@ -39,29 +39,91 @@ public class MainMenu : INavigationProvider
     //        );
     //}
 
+    //public Task BuildNavigationAsync(string name, NavigationBuilder builder)
+    //{
+    //    if (!String.Equals(name, "main", StringComparison.OrdinalIgnoreCase))
+    //    {
+    //        return Task.CompletedTask;
+    //    }
+    //    if (_httpContext.HttpContext.User.Identity.IsAuthenticated)
+    //    {
+    //        if (_httpContext.HttpContext.User.IsInRole("Freelancer"))
+    //        {
+    //            builder
+    //                 .Add(S["Freelancer Profile"], S["FreelancerProfile"], layers => layers
+    //                .Action("Index", "FreelancerProfile")
+    //                .LocalNav());
+    //        }
+    //        else if (_httpContext.HttpContext.User.IsInRole("Company"))
+    //        {
+    //            builder
+    //                 .Add(S["CompanyProfile"], S["Comapany Profile"], layers => layers
+    //                .Action("Index", "CompanyProfile")
+    //                .LocalNav());
+    //        }
+    //    }
+
+    //    return Task.CompletedTask;
+    //}
+
     public Task BuildNavigationAsync(string name, NavigationBuilder builder)
     {
-        if (!String.Equals(name, "main", StringComparison.OrdinalIgnoreCase))
+        if (!String.Equals(name, "main-menu", StringComparison.OrdinalIgnoreCase))
         {
             return Task.CompletedTask;
         }
-        if (_httpContext.HttpContext.User.Identity.IsAuthenticated)
+
+        if (_httpContext.HttpContext.User.IsInRole("Administrator"))
         {
-            if (_httpContext.HttpContext.User.IsInRole("Freelancer"))
-            {
-                builder
-                     .Add(S["Freelancer Profile"], S["FreelancerProfile"], layers => layers
-                    .Action("Index", "FreelancerProfile")
-                    .LocalNav());
-            }
-            else if (_httpContext.HttpContext.User.IsInRole("Company"))
-            {
-                builder
-                     .Add(S["CompanyProfile"], S["Comapany Profile"], layers => layers
-                    .Action("Index", "CompanyProfile")
-                    .LocalNav());
-            }
+            builder.Remove(c => c.Position == "FreelancerDashboard");
+            builder.Remove(c => c.Position == "CompanyDashboard");
+            return Task.CompletedTask;
         }
+
+        #region Freelancing
+        //builder
+        //     .Add(S["Freelancer Profile"], S["FreelancerProfile"], layers => layers
+        //    .Action("Index", "FreelancerProfile", new { area = "FrogsNetwork.Freelancing" })
+        //    .Permission(FrogsNetwork.Freelancing.Permissions.ManageFreelancerProfile)
+        //    .AddClass("nav-link")
+        //    .LocalNav());
+
+        //builder
+        //     .Add(S["Company Profile"], S["ComapanyProfile"], layers => layers
+        //    .Action("Index", "CompanyProfile", new { area = "FrogsNetwork.Freelancing" })
+        //     .Permission(FrogsNetwork.Freelancing.Permissions.ManageCompanyProfile)
+        //     .AddClass("nav-link")
+        //    .LocalNav());
+        //builder
+        //     .Add(S["Search Freelancers"], S["SearchFreelancers"], layers => layers
+        //    .Action("Index", "FreelancerSearch", new { area = "FrogsNetwork.Freelancing" })
+        //     .Permission(FrogsNetwork.Freelancing.Permissions.ManageCompanyProfile)
+        //     .AddClass("nav-link")
+        //    .LocalNav());
+        #endregion
+
+        builder
+             .Add(S["Freelancer Dashboard"], S["FreelancerDashboard"], layers => layers
+            .Action("Index", "FreelancerDashboard", new { area = "FrogsNetwork.Freelancing" })
+            .Permission(FrogsNetwork.Freelancing.Permissions.ManageFreelancerProfile)
+            .AddClass("nav-link")
+            .LocalNav());
+
+        builder
+             .Add(S["Company Dashboard"], S["CompanyDashboard"], layers => layers
+            .Action("Index", "CompanyDashboard", new { area = "FrogsNetwork.Freelancing" })
+            .Permission(FrogsNetwork.Freelancing.Permissions.ManageCompanyProfile)
+            .AddClass("nav-link")
+            .LocalNav());
+
+        //#region Forum
+        //builder
+        //     .Add(S["Forums"], S["Forums"], layers => layers
+        //    .Action("Index", "Forum", new { area = "FrogsNetwork.Forum" })
+        //    .Permission(FrogsNetwork.Forum.Permissions.ManageForum)
+        //    .AddClass("nav-link")
+        //    .LocalNav());
+        //#endregion
 
         return Task.CompletedTask;
     }
